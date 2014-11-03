5e3d97341154feb7479b661a6c674b28e5861a58
Galen Gidman <galen@galengidman.com>
2014-11-03 16:32:10 -0600

    prepped for wp.org

--- gravity-forms-dashicon.php
+++ gravity-forms-dashicon.php
@@ -1,29 +1,26 @@
 <?php
-/**
- * Plugin Name: Gravity Forms Dashicon
- * Plugin URI:  http://galengidman.com/plugins/gravity-forms-dashicon/
- * Description: Replaces the default Gravity Forms menu icon with a dashicon.
- * Version:     1.0.0
- * Author:      Galen Gidman
- * Author URI:  http://galengidman.com/
- * License:     GPL3
- */
+/*
+Plugin Name: Gravity Forms Dashicon
+Plugin URI:  http://galengidman.com/plugins/gravity-forms-dashicon/
+Description: Replaces the default Gravity Forms menu icon with a dashicon.
+Author:      Galen Gidman
+Author URI:  http://galengidman.com/
+Version:     1.0
+*/
 
 /**
- * CSS
- *
  * Adds the necessary CSS to the admin head to replace the GF menu icon with a dashicon.
  */
 function gfd_css() {
 
-?>
+  ?>
 
   <style>
     .toplevel_page_gf_edit_forms img { display: none; }
     .toplevel_page_gf_edit_forms > .dashicons-before:before { content: '\f175'; }
   </style>
 
-<?php
+  <?php
 
 }
 add_action( 'admin_head', 'gfd_css' );
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Gravity Forms Dashicon ===
+Contributors: galengidman
+Tags: icon, ui, dashicon, dash icon, menu icon, gravity forms, gravity forms menu icon, gravity forms dashicon
+Requires at least: 3.8
+Tested up to: 4.0
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Replaces the default Gravity Forms menu icon with a Dashicon.
+
+== Description ==
+
+Replaces the default Gravity Forms menu icon with a Dashicon. It has no use other than to make the admin look nicer. It was ranked #27 in Forbes' list of the top 100 most OCD-induced WordPress plugins.
+
+== Installation ==
+
+1. Upload and activate the plugin.
+
+== Frequently Asked Questions ==
+
+= Why not wait for Gravity Forms to do this themselves? =
+
+They're taking too long and I found the out-of-place icon annoying. Great product. Poor icon.
+
+== Changelog ==
+
+= 1.0 =
+* First release
